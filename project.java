9172297ae523f905e940745a959741d22d748500
Marco Mercado <mmerc007@ucr.edu>
2017-11-24 20:32:14 -0800

    getting input and reading file

---
+++ project.java
@@ -0,0 +1,52 @@
+// Java program to demonstrate positional access
+// operations on List interface
+import java.util.*;
+import java.util.Scanner;
+import java.io.*;
+
+
+public class project{
+      public static void main (String args[]){
+      List<Double> Labels = new ArrayList<Double>();
+      List<String> lines =  new ArrayList<String>();
+      double mean  = 0;
+      Scanner scan = new Scanner(System.in);
+      String fileName = scan.nextLine();
+      try {
+      			File file = new File(fileName);
+      			FileReader fileReader = new FileReader(file);
+      			BufferedReader bufferedReader = new BufferedReader(fileReader);
+      			StringBuffer stringBuffer = new StringBuffer();
+      			String line;
+      			while ((line = bufferedReader.readLine()) != null) {
+              String[] data = line.split("\\s+");
+              lines.add(line);
+              Labels.add(Double.parseDouble(data[1]));
+
+
+      	    }
+    		} catch (IOException e) {
+    			e.printStackTrace();
+    		}
+        String forSize = lines.get(0);
+        String[] FeatureSize =   forSize.split("\\s+");
+        Double[][] features = new Double[lines.size()][FeatureSize.length - 2];
+
+        for(int i = 0; i < lines.size(); i++){
+          String temp = lines.get(i);
+          String[] firstLine =   temp.split("\\s+");
+          for(int j = 2; j <firstLine.length; j++){
+            features[i][j] = Double.parseDouble( firstLine[i] );
+
+          }
+
+        }
+        // Features.add(Double.parseDouble( data[i] ) );
+        // mean = mean + data[i];
+        // String[] data = line.split("\\s+");
+        // System.out.println(data[2]);
+    }
+
+
+
+ }
