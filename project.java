7016c0c4245627ccea2ddb0030fad73d3866bef2
Marco Mercado <mmerc007@ucr.edu>
2017-11-29 10:07:01 -0800

    the data is normalize

--- project.java
+++ project.java
@@ -3,11 +3,16 @@
 import java.util.*;
 import java.util.Scanner;
 import java.io.*;
-
+import java.lang.Math;
 
 public class project{
 
-    public static void NormalizeData() {
+    public double NearestNeighbor(Double[][] data,int []features) {
+
+      double foo = 0.0;
+
+
+      return foo;
          // Do something here
      }
 
@@ -16,6 +21,7 @@ public class project{
       List<Double> Labels = new ArrayList<Double>();
       List<String> lines =  new ArrayList<String>();
       double mean  = 0;
+      int sizeF = 0;
       Scanner scan = new Scanner(System.in);
       String fileName = scan.nextLine();
       try {
@@ -35,16 +41,44 @@ public class project{
         String forSize = lines.get(0);
         String[] FeatureSize =   forSize.split("\\s+");
         Double[][] features = new Double[lines.size()][FeatureSize.length - 2];
+
         for(int i = 0; i < lines.size(); i++){
             String temp = lines.get(i);
             String[] firstLine =   temp.split("\\s+");
+            sizeF = firstLine.length;
             for(int j = 2; j < firstLine.length; j++){
               mean= Double.parseDouble( firstLine[j] ) +mean;
               features[i][j-2] = Double.parseDouble( firstLine[j] );
             }
         }
         mean = mean/ ((FeatureSize.length - 2) * lines.size());
+        double stDiv = 0;
+        for(int i = 0; i < Labels.size(); i++){
+          for(int j = 2; j < sizeF; j++){
+            stDiv = stDiv + Math.pow(features[i][j-2] - mean,2.0);
+          }
+        }
+        stDiv = stDiv / ((FeatureSize.length - 2) * lines.size());
+        stDiv = Math.sqrt (stDiv);
+        for(int i = 0; i < Labels.size(); i++){
+          for(int j = 2; j < sizeF; j++){
+            stDiv = stDiv + Math.pow(features[i][j-2] - mean,2.0);
+          }
+        }
+        for(int i = 0; i < Labels.size(); i++){
+          for(int j = 2; j < sizeF; j++){
+            features[i][j-2]= (features[i][j-2] - mean)/ stDiv;
+          }
+        }
+
+        // int[] f =  new int[
+        // NearestNeighbor(features, )
+
+
+
+
+
+
+        System.out.print(mean);
     }
-
-
- }
+  }
